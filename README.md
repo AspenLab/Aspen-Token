6114d62643ca9e569194422c4c851b65fbfc512a
mathai808 <mathai@bitstrapped.com>
2021-07-21 22:30:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aspen-Token 
+
+To read more on the Tokenomics of the Aspen ERC20 Token visit: https://aspenlabs1.medium.com/
---
+++ contracts/aspen-erc20.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+import "./ERC20.sol";
+import "./extensions/ERC20Capped.sol";
+
+
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract AspenToken is ERC20Capped {
+    
+   address public owner;
+   
+   
+    modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+}
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor ()  ERC20Capped(45000000000000000000000000) {
+        ERC20._mint(msg.sender, 15000000000000000000000000);
+        owner = msg.sender;
+    }
+    
+    function mintAspenTokens(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to,_amount);
+    }
+   
+}
